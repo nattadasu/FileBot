d62d750b109f31241e56e66d7c648735a4e6150b
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-07-20 15:55:09 +0700

    Refactor filespec.groovy

--- filespec.groovy
+++ filespec.groovy
@@ -1,7 +1,8 @@
- [
+{" ["}
 @./releasesource.groovy
 {resolution} {vcf} {bitdepth}Bit, {ac}
- {af.format(
+{" "}
+{af.format(
     8: 'DD+ 7.1',
     7: '6.1',
     6: 'DD 5.1',
@@ -16,11 +17,15 @@
     def langs_ = audioLanguages.size() > 5 ? audioLanguages.take(5) : audioLanguages
     substat ? substat + langs_.joining(" ", " (", "").upper() + {audioLanguages.size() > 5 ? " ...)" : ")"}: ""
 }
-{def n = any{textLanguages.size()}{0}
+{
+    def n = any{textLanguages.size()}{0}
     def substat = n > 2 ? ", Multi-Subs" : n > 1 ? ", Dual-Subs" : null
     def langs_ = textLanguages.size() > 5 ? textLanguages.take(5) : textLanguages
     substat ? substat + langs_.joining(" ", " (", "").upper() + {textLanguages.size() > 5 ? " ...)" : ")"}: ""
 }
 ][{crc32.upper()}]
 {ext =~ /(ass|srt|ssa|vtt)/ ? '.' + lang.ISO3B: ""}
-{ext =~ /jp(?:e)?g|png/ ? "-thumb" : ""}
+{
+    // Jellyfin 10.9.* default thumbnail name
+    ext =~ /jp(?:e)?g|png/ ? "-thumb" : ""
+}
