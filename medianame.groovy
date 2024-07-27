821dc212f03b969079b23f85f2c5f1251626b9ea
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-07-27 20:37:27 +0700

    Add Cherry Magic! to Alt name

--- medianame.groovy
+++ medianame.groovy
@@ -2,7 +2,8 @@
     def short_title = [
         [series_id: 229743, title: "VTuber Legend"],
         [series_id: 240633, title: "A Journey Through Another World"],
-        [series_id: 245285, title: "Failure Frame"]
+        [series_id: 245285, title: "Failure Frame"],
+        [series_id: 237045, title: "Chery Magic!"]
     ]
 
     // Replace title from short_title if series_id matches "series" from filebot
