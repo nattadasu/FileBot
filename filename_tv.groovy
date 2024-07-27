389111a1810cfda6e990e53814e2afb15e2fcd65
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-07-28 01:53:40 +0700

    Make sure script to be futureproof
    Who knows I'm implementing minifier?

--- filename_tv.groovy
+++ filename_tv.groovy
@@ -1,7 +1,7 @@
 @./releasegroup.groovy
 {" "}
 @./medianame.groovy
- - {s00e00} - 
+{" - "}{s00e00}{" - "}
 {
     def invalid_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
     def fixed_name = t.replaceAll(invalid_chars.collect { "\\" + it }.join('|'), '_')
