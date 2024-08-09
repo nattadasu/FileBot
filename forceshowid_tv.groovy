bd2ecd32871f132797cd1270d43e9e5d2c555f3f
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-08-10 01:54:55 +0700

    Add Kimi ni Todoke proper formatting

--- forceshowid_tv.groovy
+++ forceshowid_tv.groovy
@@ -1,6 +1,6 @@
 {
 	def show_id = [
-		239779, 112888
+		68854
 	]
 	def is_id_matches = show_id.find { curr_id -> tmdbid == curr_id } ?: ""
     is_id_matches ? " ($y) [tmdbid-$is_id_matches]" : ""
--- medianame.groovy
+++ medianame.groovy
@@ -3,8 +3,8 @@
         [series_id: 229743, title: "VTuber Legend"],
         [series_id: 240633, title: "A Journey Through Another World"],
         [series_id: 245285, title: "Failure Frame"],
-        [series_id: 237045, title: "Chery Magic!"],
-        [series_id: 260840, title: "Whimsical Wish"]
+        [series_id: 237045, title: "Cherry Magic!"],
+        [series_id:  68854, title: "From Me to You"]
     ]
 
     // Replace title from short_title if series_id matches "series" from filebot
