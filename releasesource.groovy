a3826cca1b47c7f4bf7f02c3334e68aeb8d23c5a
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-10-09 21:09:53 +0700

    Add B-Global synonym

--- releasesource.groovy
+++ releasesource.groovy
@@ -11,7 +11,7 @@
     def finalRelease = customRelease.find { releaseName -> fn.contains(releaseName) } ?: ""
 
     def release_ = any {
-      fn.contains("BiliIntl") ? "B-Global.WEB-DL" : ""
+      fn.contains("BiliIntl") || fn.contains("BILI") ? "B-Global.WEB-DL" : ""
     } {
         finalRelease ? finalRelease + ".WEB-DL" : ""
     } {
