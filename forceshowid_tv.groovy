19b87cfc7c73ae5e42936b18a66ba1c6c72427fb
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-10-09 21:10:09 +0700

    Add Ranma 1/2 (2024)

--- forceshowid_tv.groovy
+++ forceshowid_tv.groovy
@@ -1,9 +1,10 @@
 {
-	def show_id = [
-		68854,  // Kimi ni Todoke (2009) (君に届け), JP
-		235973, // Tom and Jerry (2022) (とむとじぇりー), JP
-		262295, // Roommates (2024) (戏精宿舍), CN
-	]
-	def is_id_matches = show_id.find { curr_id -> tmdbid == curr_id } ?: ""
+    def show_id = [
+        68854,  // Kimi ni Todoke (2009) (君に届け), JP
+        235973, // Tom and Jerry (2022) (とむとじぇりー), JP
+        262295, // Roommates (2024) (戏精宿舍), CN
+        259140, // Ranma 1/2 (2024), JP
+    ]
+    def is_id_matches = show_id.find { curr_id -> tmdbid == curr_id } ?: ""
     is_id_matches ? " ($y) [tmdbid-$is_id_matches]" : ""
 }
