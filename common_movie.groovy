1547d3b9e238421e2d9efe9f139e8e04f65f0e6c
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-10-09 21:10:58 +0700

    Add exception handler when metadata is missing

--- common_movie.groovy
+++ common_movie.groovy
@@ -2,7 +2,7 @@
     // check Country of Origin
     def cjk_countries = /(CN|JP|KR|KP|TW|HK)/
     def cjkani_tags = /(aeni|donghua|anime)/
-    def is_anime = anime || info.Keywords =~ cjkani_tags || (genres =~ /Animation/ && country =~ cjk_countries) ? true : false
+    def is_anime = anime || info.Keywords =~ cjkani_tags || any {(genres =~ /Animation/ && country =~ cjk_countries)}{false} ? true : false
 
     // Categorized path
     def cust_cat = is_anime ? "Videos/Anime " : "Videos/"
