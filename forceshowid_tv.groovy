438026cb6b1d80d058185d2f4577c4b153b68dcf
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-10-24 17:53:37 +0700

    Add Ya She, Taisho Era Contract Marriage, ...
    and Trillion Game to force title matching (for Jellyfin)

--- forceshowid_tv.groovy
+++ forceshowid_tv.groovy
@@ -6,6 +6,9 @@
         259140, // Ranma 1/2 (2024) (らんま1/2), JP
         204098, // Murai in Love (2024) (村井の恋), JP
         239779, // True Beauty (2024) (여신강림), KR
+        273190, // Ya She/Silent House (2024) (哑舍), ZH
+        271026, // Taisho Era Contact Marriage (2024) (大正偽りブラヰダル～身代わり花嫁と軍服の猛愛), JP
+        240125, // Trillion Game (2024) (トリリオンゲーム), JP
     ]
     def is_id_matches = show_id.find { curr_id -> tmdbid == curr_id } ?: ""
     is_id_matches ? " ($y) [tmdbid-$is_id_matches]" : ""
