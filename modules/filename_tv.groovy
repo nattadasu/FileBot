0ac8ffec5f899913998d38afbfc882355fa6d552
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-10-28 17:42:04 +0700

    Remove episode title if more than 100 chars

--- modules/filename_tv.groovy
+++ modules/filename_tv.groovy
@@ -1,9 +1,10 @@
 @./releasegroup.groovy
 @./medianame.groovy
-{" - "}{s00e00}{" - "}
+{" - "}{s00e00}
 {
     def invalid_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
     def fixed_name = t.replaceAll(invalid_chars.collect { "\\" + it }.join('|'), '_')
-    fixed_name
+    // check if fixed name length is more than 100, if so, empty it
+    fixed_name.length() > 100 ? " " : " - $fixed_name "
 }
 @./filespec.groovy
