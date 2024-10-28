b5e3da702fa8d728c8c86d94e9c2e7cf046d26de
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-10-28 17:45:41 +0700

    Replace horizonal tab (\t) to spaces for indent

--- modules/season.groovy
+++ modules/season.groovy
@@ -5,8 +5,8 @@
 {
     def invalid_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
     def fixed_name = sn.replaceAll(invalid_chars.collect { "\\" + it }.join('|'), '_')
-	def fixed_title = n.replaceAll(invalid_chars.collect { "\\" + it }.join('|'), '_')
-	def kv = fixed_name == fixed_title ? "" : fixed_name
+    def fixed_title = n.replaceAll(invalid_chars.collect { "\\" + it }.join('|'), '_')
+    def kv = fixed_name == fixed_title ? "" : fixed_name
     kv == 'Season '+ s ? "" : kv ? " - " + kv : ""
 }
 {"/"}
