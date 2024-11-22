c71f4d068d9d33e2c52bdbf2c09c0995ab6f24e6
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-11-22 11:04:50 +0700

    Fix manual source override for unknown groups, add Netflix tag

--- modules/releasesource.groovy
+++ modules/releasesource.groovy
@@ -8,27 +8,31 @@
         [platform: "Hulu", aliases: ["HULU"]],
         [platform: "Shahid", aliases: ["SHAHID"]],
         [platform: "YouTube", aliases: ["YT.WEB-DL", "YOUTUBE", "YTB"]],
+        [platform: "Netflix", aliases: ["NF.WEB-DL", "NF WEB"]]
     ]
 
-    // List of groups that re-encode the video from specific source
-    def reencode_group = ["ASW", "Judas"]
+    // List of groups that re-encode or modified even further the video from specific source
+    def reencode_group = ["ASW", "Judas", "JRx7"]
     // List of groups that directly rip the video from specific source
-    def release_group = ["Erai-raws", "SubsPlease", "JRx7"]
+    def release_group = ["Erai-raws", "SubsPlease"]
+    def unlisted_group = (fn =~ /^\[(.*?)\]/)[0][1]
 
     // Find the release source from the custom releases list on filename (fn)
 
     def release_ = any {
         def allas = custom_releases.find { crate -> crate.aliases.find { aka -> fn.contains(aka) } }?.aliases.first() ?: ""
-        allas = allas.replace("YT.WEB-DL", "YT")
+        allas = allas.replace(".WEB-DL", "")
         allas ? "${allas}.WEB-DL" : ""
     } {
         source
     } {
-        reencode_group.find { g_ -> group == g_ } ? "WEBRip" : ""
+        reencode_group.find { g_ -> group == g_ || unlisted_group == g_ } ? "WEBRip" : ""
     } {
-        release_group.find { r_ -> group == r_ } ? "WEB-DL" : ""
+        release_group.find { r_ -> group == r_ || unlisted_group == r_} ? "WEB-DL" : ""
     } {
-        ""
+        vs
+    } {
+    	""
     }
 
     release_ ? "$release_ " : ""
