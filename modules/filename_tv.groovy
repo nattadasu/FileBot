6489ec3a3d488c3d9564bb2dcd3eb2190f3c0d85
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-12-01 10:33:57 +0700

    Remove default episode name if found

--- modules/filename_tv.groovy
+++ modules/filename_tv.groovy
@@ -4,7 +4,9 @@
 {
     def invalid_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
     def fixed_name = t.replaceAll(invalid_chars.collect { "\\" + it }.join('|'), '_')
+    def epnum = any { e } { special }
     // check if fixed name length is more than 100, if so, empty it
-    fixed_name.length() > 100 ? " " : " - $fixed_name "
+    fixed_name = fixed_name.length() > 100 ? " " : " - $fixed_name "
+    fixed_name == " - Episode $epnum " ? " " : fixed_name
 }
 @./filespec.groovy
