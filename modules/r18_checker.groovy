f1fa0e404f8daf536cbb3e1cb8d662350bc64d83
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-12-07 01:06:00 +0700

    Add R18+ overrider for misclassified titles

--- modules/r18_checker.groovy
+++ modules/r18_checker.groovy
@@ -12,11 +12,18 @@
         [220118, "TheMovieDB::TV"], // Sazanami Soushi ni Junketsu wo Sasagu, 2023
     ]
 
+    // List of known titles that might be misclassified as R18+
+    def override_tag = [
+        [236530, "TheMovieDB::TV"] // How I Attended an All-Guy's Mixer, 2024
+    ]
+
     // REQUIRES USER TO SET ADDITIONAL PROPERTY ON FILEBOT
     // READ: https://www.filebot.net/forums/viewtopic.php?p=58610#p58610
     def is_adult = any { info.adult } { false }
 
     def is_same_id = known_titles.find { it[0] == id && it[1] == info.database } != null
+    def is_adult_override = override_tag.find { it[0] == id && it[1] == info.database } != null
+    is_same_id = is_adult_override ? false : is_same_id
     def is_r18 = info.Keywords.findAll { it in known_tags } || is_same_id || is_adult
     is_r18 ? "R18+ " : ""
 }
