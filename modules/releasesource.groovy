f5d39ef70ee3cb900d1185387b9dc006fe141b83
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-12-12 10:12:37 +0700

    Refactor and simplify platform alias handling

--- modules/releasesource.groovy
+++ modules/releasesource.groovy
@@ -1,18 +1,18 @@
 {
     def custom_releases = [
         // Platform name is for internal use only, aliases are used to match the filename
-        [platform: "ABEMA", aliases: ["ABEMA"]], // https://abema.tv/
-        [platform: "Bilibili.tv", aliases: ["BILI", "B-Global", "BiliIntl", "B-Site", "BiliCN"]], // https://www.bilibili.tv and https://www.bilibili.com
-        [platform: "Hulu", aliases: ["HULU"]], // https://www.hulu.com and https://www.hulu.jp
-        [platform: "iQIYI", aliases: ["IQ.WEB-DL", "IQIYI"]], // https://www.iq.com
-        [platform: "Laftel.tv", aliases: ["LAFTEL", "LFTL"]], // https://laftel.tv
-        [platform: "MeWatch", aliases: ["MW.WEB-DL"]], // https://www.mewatch.sg
-        [platform: "Netflix", aliases: ["NF.WEB-DL", "NF WEB"]], // https://www.netflix.com
-        [platform: "Prime Video", aliases: ["AMZN"]], // https://www.primevideo.com
-        [platform: "Shahid", aliases: ["SHAHID"]], // https://shahid.mbc.net
-        [platform: "TVer", aliases: ["TVER"]], // https://tver.jp
-        [platform: "U-NEXT", aliases: ["UNEXT"]], // https://video.unext.jp
-        [platform: "YouTube", aliases: ["YT.WEB-DL", "YOUTUBE", "YTB"]], // https://www.youtube.com
+        ["ABEMA"], // https://abema.tv/
+        ["BILI", "B-Global", "BiliIntl", "B-Site", "BiliCN"], // https://www.bilibili.tv and https://www.bilibili.com
+        ["HULU"], // https://www.hulu.com and https://www.hulu.jp
+        ["IQIYI", "IQ.WEB-DL"], // https://www.iq.com
+        ["LAFTEL", "LFTL"], // https://laftel.tv
+        ["MW.WEB-DL"], // https://www.mewatch.sg
+        ["NF.WEB-DL", "NF WEB"], // https://www.netflix.com
+        ["AMZN"], // https://www.primevideo.com
+        ["SHAHID"], // https://shahid.mbc.net
+        ["TVER"], // https://tver.jp
+        ["UNEXT"], // https://video.unext.jp
+        ["YT.WEB-DL", "YOUTUBE", "YTB"], // https://www.youtube.com
     ]
 
     // List of groups that re-encode or modified even further the video from specific source
@@ -23,9 +23,10 @@
 
     // Find the release source from the custom releases list on filename (fn)
     def release_ = any {
-        def allas = custom_releases.find { crate -> crate.aliases.find { aka -> fn.contains(aka) } }?.aliases.first() ?: ""
-        allas = allas.replace(".WEB-DL", "")
-        allas ? "${allas}.WEB-DL" : ""
+        def platform = custom_releases.find { idx -> idx.find { known -> fn.lower().contains(known.lower()) } }[0]
+        platform = platform.replace(".WEB-DL", "")
+        platform ? "${platform}.WEB-DL" : ""
+    } {
     } {
         source
     } {
