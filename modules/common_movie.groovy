2658b7fe1fb73617b5bc71e026136ae909282a7b
Natsu Tadama <nattadasu@nattadasu.my.id>
2025-01-03 18:48:41 +0700

    Resolve missing slash to separate directory to file content

--- modules/common_movie.groovy
+++ modules/common_movie.groovy
@@ -18,6 +18,7 @@
     ]
     def db_src = db_map[db_name]
     def fixed_id = db_name == "IMDb" ? "tt${id}" : id
-    " [${db_src}-${fixed_id}]/" */
+    " [${db_src}-${fixed_id}]" */
 }
+{"/"}
 @./filename_movie.groovy
