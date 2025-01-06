318743f2d7926f03df8322da844fac103ef7632c
Natsu Tadama <nattadasu@nattadasu.my.id>
2025-01-06 21:22:28 +0700

    Lock この会社に好きな人がいます English name, although incorrect...

--- modules/medianame.groovy
+++ modules/medianame.groovy
@@ -7,6 +7,7 @@
         [mid: 245561, title: "Okitsura", src: tmdb_tv],
         [mid: 248707, title: "Neko Oji The Guy That Got Reincarnated As a Cat", src: tmdb_tv],
         [mid: 259559, title: "Headhunted to Another World", src: tmdb_tv],
+        [mid: 262141, title: "Can You Keep a Secret", src: tmdb_tv],
         [mid: 276204, title: "Bogus Skill Fruitmaster", src: tmdb_tv],
     ]
 
