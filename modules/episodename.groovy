413d2e5731ffa7fbc150498b0b48bb5922640200
Natsu Tadama <nattadasu@nattadasu.my.id>
2025-01-09 23:50:12 +0700

    Split function to separate folder for episode name patch

---
+++ modules/episodename.groovy
@@ -0,0 +1,9 @@
+{" - "}{s00e00}
+{
+    def invalid_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
+    def fixed_name = t.replaceAll(invalid_chars.collect { "\\" + it }.join('|'), '_')
+    def epnum = any { e } { special }
+    // check if fixed name length is more than 100, if so, empty it
+    fixed_name = fixed_name.length() > 100 ? " " : " - $fixed_name "
+    fixed_name == " - Episode $epnum " ? " " : fixed_name
+}
--- modules/filename_tv.groovy
+++ modules/filename_tv.groovy
@@ -1,12 +1,4 @@
 @./releasegroup.groovy
 @./medianame.groovy
-{" - "}{s00e00}
-{
-    def invalid_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
-    def fixed_name = t.replaceAll(invalid_chars.collect { "\\" + it }.join('|'), '_')
-    def epnum = any { e } { special }
-    // check if fixed name length is more than 100, if so, empty it
-    fixed_name = fixed_name.length() > 100 ? " " : " - $fixed_name "
-    fixed_name == " - Episode $epnum " ? " " : fixed_name
-}
+@./episodename.groovy
 @./filespec.groovy
