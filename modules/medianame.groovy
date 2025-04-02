bc805873d3e48513341b126c031e26d5601ca7ac
Natsu Tadama <nattadasu@nattadasu.my.id>
2025-04-03 03:10:05 +0700

    Add The Too-Perfect Saint

--- modules/medianame.groovy
+++ modules/medianame.groovy
@@ -2,17 +2,11 @@
     def tmdb_tv = "TheMovieDB::TV"
     def short_title = [
         // Media ID, target name, database
-        [mid:  68854, title: "From Me to You", src: tmdb_tv],
-        [mid: 100937, title: "Crazy Over His Fingers", src: tmdb_tv],
         [mid: 199928, title: "Nights With a Cat", src: tmdb_tv],
         [mid: 219816, title: "Fate Grand Order Fujimaru Ritsuka Doesn't Get It", src: tmdb_tv],
-        [mid: 245561, title: "Okitsura", src: tmdb_tv],
         [mid: 248707, title: "Neko Oji The Guy That Got Reincarnated As a Cat", src: tmdb_tv],
-        [mid: 259559, title: "Headhunted to Another World", src: tmdb_tv],
-        [mid: 261301, title: "Botsuraku Kizoku", src: tmdb_tv],
-        [mid: 262141, title: "Can You Keep a Secret", src: tmdb_tv],
         [mid: 270657, title: "Tono to Inu", src: tmdb_tv],
-        [mid: 276204, title: "Bogus Skill Fruitmaster", src: tmdb_tv],
+        [mid: 274741, title: "The Too-Perfect Saint", src: tmdb_tv]
     ]
 
     // Replace title from short_title if series_id matches "series" from filebot
