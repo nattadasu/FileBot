556bb859a451ee9df8c145720e3cd801a3ade694
Natsu Tadama <nattadasu@nattadasu.my.id>
2025-06-11 15:05:54 +0700

    Comment out Tom & Jerry Gokko forced ID

--- modules/forceshowid_tv.groovy
+++ modules/forceshowid_tv.groovy
@@ -3,7 +3,7 @@
     def show_id = [
     	[ 95897, tmdb_tv], // Overflow (2020) (おーばーふろぉ), JP
         [123542, tmdb_tv], // LINK CLICK (2021) (时光代理人), ZH, Donghua
-        [235973, tmdb_tv], // Tom and Jerry (2022) (とむとじぇりー), JP
+        //[235973, tmdb_tv], // Tom and Jerry (2022) (とむとじぇりー), JP
         [239779, tmdb_tv], // True Beauty (2024) (여신강림), KR
         [257162, tmdb_tv], // LINK CLICK (2024) (时光代理人), ZH, Live Action
         [259140, tmdb_tv], // Ranma 1/2 (2024) (らんま1/2), JP
