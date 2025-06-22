040fdaa32ea45f84e375c9ab4825470ef84f0af1
Natsu Tadama <nattadasu@nattadasu.my.id>
2025-06-22 20:16:29 +0700

    Add Pochars to media alias

--- modules/medianame.groovy
+++ modules/medianame.groovy
@@ -5,6 +5,7 @@
         [mid: 199928, title: "Nights With a Cat", src: tmdb_tv],
         [mid: 219816, title: "Fate Grand Order Fujimaru Ritsuka Doesn't Get It", src: tmdb_tv],
         [mid: 248707, title: "Neko Oji The Guy That Got Reincarnated As a Cat", src: tmdb_tv],
+        [mid: 270660, title: "Pochars", src: tmdb_tv],
         [mid: 271003, title: "Yandere Dark Elf", src: tmdb_tv],
         [mid: 274741, title: "The Too-Perfect Saint", src: tmdb_tv],
         [mid: 275623, title: "Chuzenji-sensei", src: tmdb_tv],
