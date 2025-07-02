8236e1b0cc5995e0989e75a7917ae66bbf241cfb
Natsu Tadama <nattadasu@nattadasu.my.id>
2025-07-03 03:53:35 +0700

    Bruh… Again??

--- modules/medianame.groovy
+++ modules/medianame.groovy
@@ -6,7 +6,7 @@
         [mid: 248707, title: "Neko Oji The Guy That Got Reincarnated As a Cat", src: tmdb_tv],
         [mid: 270660, title: "Pochars", src: tmdb_tv],
         [mid: 280078, title: "The All-devouring Whale Homecoming", src: tmdb_tv],
-        [mid; 283884: title: "Chuhai Lips", src: tmdb_tv],
+        [mid: 283884, title: "Chuhai Lips", src: tmdb_tv],
         [mid: 288419, title: "Chikuwa Senki", src: tmdb_tv],
     ]
 
