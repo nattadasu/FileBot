d45108adf38cc7fe83b55afdf1fafe3b2c0d55ec
Natsu Tadama <nattadasu@nattadasu.my.id>
2025-10-07 23:02:41 +0700

    Force ID on Debu to Love Anime

--- modules/forceshowid_tv.groovy
+++ modules/forceshowid_tv.groovy
@@ -4,10 +4,9 @@
     	[ 95897, tmdb_tv], // Overflow (2020) (おーばーふろぉ), JP
         [123542, tmdb_tv], // LINK CLICK (2021) (时光代理人), ZH, Donghua
         //[235973, tmdb_tv], // Tom and Jerry (2022) (とむとじぇりー), JP
-        [239779, tmdb_tv], // True Beauty (2024) (여신강림), KR
         [257162, tmdb_tv], // LINK CLICK (2024) (时光代理人), ZH, Live Action
         [259140, tmdb_tv], // Ranma 1/2 (2024) (らんま1/2), JP
-        [276204, tmdb_tv], // Bogus Skill Fruitmaster (2025), (外れスキル《木の実マスター》), JP
+        [296287, tmdb_tv], //Plus-sized Misadventures in Love! (2025) (デブとラブと過ちと!), JP
     ]
     def is_id_matches = show_id.find { it[0] == id && it[1] == info.database } != null
     // try map TheMovieDB::TV -> tmdbid, AniDB -> anidbid, TheTVDB -> tvdbid
