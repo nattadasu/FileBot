3dc43df5aeb54a3ae59d2bdaf2bc126d78d1b94d
Natsu Tadama <nattadasu@nattadasu.my.id>
2025-10-28 00:17:55 +0700

    Update media names

--- modules/forceshowid_tv.groovy
+++ modules/forceshowid_tv.groovy
@@ -3,6 +3,7 @@
     def show_id = [
     	[ 95897, tmdb_tv], // Overflow (2020) (おーばーふろぉ), JP
         [123542, tmdb_tv], // LINK CLICK (2021) (时光代理人), ZH, Donghua
+        [129633, tmdb_tv], // Super Secret (2020) (슈퍼 시크릿), KR, Aeni
         //[235973, tmdb_tv], // Tom and Jerry (2022) (とむとじぇりー), JP
         [257162, tmdb_tv], // LINK CLICK (2024) (时光代理人), ZH, Live Action
         [259140, tmdb_tv], // Ranma 1/2 (2024) (らんま1/2), JP
