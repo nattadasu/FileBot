3238477fcaee5b7fe61a151ecc0370480149f44c
Natsu Tadama <nattadasu@nattadasu.my.id>
2026-01-04 22:19:36 +0700

    Add alias and ID forcing

--- modules/forceshowid_tv.groovy
+++ modules/forceshowid_tv.groovy
@@ -7,8 +7,9 @@
         [129633, tmdb_tv], // Super Secret (2020) (슈퍼 시크릿), KR, Aeni
         //[235973, tmdb_tv], // Tom and Jerry (2022) (とむとじぇりー), JP
         [257162, tmdb_tv], // LINK CLICK (2024) (时光代理人), ZH, Live Action
+        [295366, tmdb_tv], // Yuukawa (2026), JP
+        [296287, tmdb_tv], // Plus-sized Misadventures in Love! (2025) (デブとラブと過ちと!), JP
         [259140, tmdb_tv], // Ranma 1/2 (2024) (らんま1/2), JP
-        [296287, tmdb_tv], //Plus-sized Misadventures in Love! (2025) (デブとラブと過ちと!), JP
     ]
     def is_id_matches = show_id.find { it[0] == id && it[1] == info.database } != null
     // try map TheMovieDB::TV -> tmdbid, AniDB -> anidbid, TheTVDB -> tvdbid
